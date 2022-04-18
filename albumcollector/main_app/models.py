8875fbc5caac94c4f2e01f3d0998ed76d50b25ac
chrislee1996 <prepchrislee@gmail.com>
2022-04-18 18:44:47 -0400

    commented out views albus and was able to render my albums created via our models in my database,also  connected django and postgre

--- albumcollector/main_app/models.py
+++ albumcollector/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=100)
+    albumName = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    yearOfRelease = models.IntegerField()
+
+    def __str__(self):
+        return self.albumName
+
+
+# a = Album(artist="", albumName='', genre='',yearOfRelease=)
+# a.save()
--- albumcollector/main_app/views.py
+++ albumcollector/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Album
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -9,19 +10,20 @@ def about(request):
   return render(request, 'about.html')
 
 
-class Album:
-    def __init__(self,artist,albumName,genre,yearOfRelease):
-        self.artist = artist
-        self.albumName = albumName
-        self.genre = genre
-        self.yearOfRelease = yearOfRelease
+# class Album:
+#     def __init__(self,artist,albumName,genre,yearOfRelease):
+#         self.artist = artist
+#         self.albumName = albumName
+#         self.genre = genre
+#         self.yearOfRelease = yearOfRelease
 
-albums = [
-    Album('The Weekend', 'Dawn FM', 'R&B', 2022),
-    Album('TYLER THE CREATOR', 'IGOR', 'Hip-hop', 2019),
-    Album('Dua Lipa', 'Future Nostalgia', 'Pop', 2020)
-]
+# albums = [
+#     Album('The Weekend', 'Dawn FM', 'R&B', 2022),
+#     Album('TYLER THE CREATOR', 'IGOR', 'Hip-hop', 2019),
+#     Album('Dua Lipa', 'Future Nostalgia', 'Pop', 2020)
+# ]
 
 
 def albums_index(request):
+    albums = Album.objects.all()
     return render(request, 'albums/index.html', {'albums':albums})
