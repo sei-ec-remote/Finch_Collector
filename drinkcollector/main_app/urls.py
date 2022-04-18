737a190c69cc61573c41aeeb83e04fc5137f3c53
patrickga <patrick.g.alvarez@gmail.com>
2022-04-18 18:21:42 -0500

    Create, update, and delete routes working

--- drinkcollector/main_app/urls.py
+++ drinkcollector/main_app/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('drinks/', views.drinks_index, name='drinks_index'),
-    path('drinks/<int:drink_id>/', views.drinks_show, name='drinks_show')
+    path('drinks/<int:drink_id>/', views.drinks_show, name='drinks_show'),
+    path('drinks/create/', views.DrinkCreate.as_view(), name='drinks_create'),
+    path('drinks/<int:pk>/update/', views.DrinkUpdate.as_view(), name='drinks_update'),
+    path('drinks/<int:pk>/delete/', views.DrinkDelete.as_view(), name='drinks_delete')
 ]
