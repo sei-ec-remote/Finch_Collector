0d9e52442a55abfa76fd0ccd2bb91fa255c0d00c
chrislee1996 <prepchrislee@gmail.com>
2022-04-18 17:57:09 -0400

    rendered about and index page

--- albumcollector/main_app/views.py
+++ albumcollector/main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'index.html')
+
+def about(request):
+  return render(request, 'about.html')
