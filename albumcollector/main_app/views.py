c7bf1cd6da85fb716ca85c3db36f1787b36c525c
chrislee1996 <prepchrislee@gmail.com>
2022-04-18 18:14:31 -0400

    able to render inputted albums via index page with styling

--- albumcollector/main_app/views.py
+++ albumcollector/main_app/views.py
@@ -7,3 +7,21 @@ def index(request):
 
 def about(request):
   return render(request, 'about.html')
+
+
+class Album:
+    def __init__(self,artist,albumName,genre,yearOfRelease):
+        self.artist = artist
+        self.albumName = albumName
+        self.genre = genre
+        self.yearOfRelease = yearOfRelease
+
+albums = [
+    Album('The Weekend', 'Dawn FM', 'R&B', 2022),
+    Album('TYLER THE CREATOR', 'IGOR', 'Hip-hop', 2019),
+    Album('Dua Lipa', 'Future Nostalgia', 'Pop', 2020)
+]
+
+
+def albums_index(request):
+    return render(request, 'albums/index.html', {'albums':albums})
