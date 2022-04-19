81adc6f5ccc49f2eba5fadbd00f68f9754f29a04
NikPurvis <nikpurvis@gmail.com>
2022-04-18 18:57:26 -0600

    Added templates for index and about, modified routes

--- xmencollector/main_app/views.py
+++ xmencollector/main_app/views.py
@@ -2,12 +2,20 @@
 # Declare the routes and client responses here.
 
 from django.shortcuts import render
-from django.http import HttpResponse
+# # We're now rendering a template instead of sending an HTTP response, so we no longer need this.
+# from django.http import HttpResponse
 
 # Create your views here.
+
+# # We're now rendering a template instead of sending an HTTP response, so we no longer need these.
+# def index(request):
+#     return HttpResponse("<h1> Hi I'm the front page</h1>")
+# def about(request):
+#     return HttpResponse("<h1>I'm a giant comic book nerd how you doing</h1>")
+
 def index(request):
-    return HttpResponse("<h1> Hi I'm the front page</h1>")
+    return render(request, "index.html")
 
 def about(request):
-    return HttpResponse("<h1>I'm a giant comic book nerd how you doing</h1>")
+    return render(request, "about.html")
 
