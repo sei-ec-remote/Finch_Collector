4906653a2cd2ddb7915cb65c5fc7c39e502ff5d0
Ivycle94 <Ivycle94@gmail.com>
2022-04-18 19:58:10 -0500

    fix indentation to display name in the Django shell

--- doggos/main_app/models.py
+++ doggos/main_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -9,5 +8,5 @@ class Dog(models.Model):
     description = models.CharField(max_length=250)
     age = models.IntegerField()
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
