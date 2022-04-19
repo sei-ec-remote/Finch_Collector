f90df3b8d519cb78dea18b7a985fff00de096fdf
Ivycle94 <Ivycle94@gmail.com>
2022-04-18 20:43:10 -0500

    render for dogs index and show page working

--- doggos/main_app/views.py
+++ doggos/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Dog
 
 # Create your views here ...
 
@@ -13,18 +14,23 @@ def about(request):
     return render(request, 'about.html')
 
 # Class Constructor for Dogs (dummy data)
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Dog:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-dogs = [
-    Dog('Chibi', 'Shiba Inu', 'Spoiled', 6),
-    Dog('Yuki', 'Aussie', 'Zoomies', 4),
-    Dog('Loki', 'Aussie', 'Gudboi', 5)
-]
+# dogs = [
+#     Dog('Chibi', 'Shiba Inu', 'Spoiled', 6),
+#     Dog('Yuki', 'Aussie', 'Zoomies', 4),
+#     Dog('Loki', 'Aussie', 'Gudboi', 5)
+# ]
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
+
+def dogs_show(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/show.html', {'dog': dog})
