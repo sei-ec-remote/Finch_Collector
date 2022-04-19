04b91c050e6a839d917af13d51b13f541d77acab
katherine-kania <katherine.kania@gmail.com>
2022-04-18 21:41:20 -0400

    Index and about page rendering and running

---
+++ recipecollector/main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+# class Recipes:
+#     def __init__()
