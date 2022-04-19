4522e32dbc44ea346d3db477028f0152f741e498
jfw2855 <jwaddle1130@gmail.com>
2022-04-18 22:53:58 -0500

    adds paths to create, update and delete

--- pizzacollector/main_app/urls.py
+++ pizzacollector/main_app/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('pizza/', views.pizza_index, name='pizza_index'),
     path('pizza/<int:pizza_id>', views.pizza_show, name='pizza_show'),
+    path('pizza/create/', views.PizzaCreate.as_view(), name='pizza_create'),
+    path('pizza/<int:pk>/update/', views.PizzaUpdate.as_view(), name='pizza_update'),
+    path('pizza/<int:pk>/delete/', views.PizzaDelete.as_view(), name='pizza_delete'),
+
 
 ]
