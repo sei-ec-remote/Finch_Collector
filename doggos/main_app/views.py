3b0e9dc6e57053f9d2d659746794bf985e508236
Ivycle94 <Ivycle94@gmail.com>
2022-04-18 19:00:36 -0500

    index page for dogs now populates dummy data and view render working

--- doggos/main_app/views.py
+++ doggos/main_app/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here ...
 
+# Index
 def index(request):
     # return HttpResponse('<h1> Woof! υ´• ﻌ •`υ </h1>')
     return render(request, 'index.html')
 
+# About
 def about(request):
     return render(request, 'about.html')
 
+# Class Constructor for Dogs (dummy data)
+class Dog:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+dogs = [
+    Dog('Chibi', 'Shiba Inu', 'Spoiled', 6),
+    Dog('Yuki', 'Aussie', 'Zoomies', 4),
+    Dog('Loki', 'Aussie', 'Gudboi', 5)
+]
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {'dogs': dogs})
