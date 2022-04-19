29d80d1eacbbd91aebf33288b910371f46837b44
NikPurvis <nikpurvis@gmail.com>
2022-04-18 20:24:45 -0600

    Model successfull migrated to database

--- xmencollector/xmencollector/settings.py
+++ xmencollector/xmencollector/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'xmencollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "issues",
+        "USER": "postgres"
     }
 }
 
