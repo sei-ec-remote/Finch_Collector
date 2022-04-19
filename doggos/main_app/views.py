9cc9827db103309de80a4007c6c53eb64cc46bab
Ivycle94 <Ivycle94@gmail.com>
2022-04-18 21:43:34 -0500

    create and delete round full rendering and working

--- doggos/main_app/views.py
+++ doggos/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here ...
