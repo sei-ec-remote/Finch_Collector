fe68c544eec8e8cc801833741143ae55ab191a1a
jfw2855 <jwaddle1130@gmail.com>
2022-04-18 21:31:36 -0500

    adds path to pizza index

--- pizzacollector/main_app/urls.py
+++ pizzacollector/main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('pizza/', views.pizza_index, name='pizza_index'),
 
 ]
