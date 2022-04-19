99e3b5133a4f619aa7dffa9c8585396d26b45725
NikPurvis <nikpurvis@gmail.com>
2022-04-18 18:46:35 -0600

    About page connected and working

--- xmencollector/main_app/views.py
+++ xmencollector/main_app/views.py
@@ -8,3 +8,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1> Hi I'm the front page</h1>")
 
+def about(request):
+    return HttpResponse("<h1>I'm a giant comic book nerd how you doing</h1>")
+
