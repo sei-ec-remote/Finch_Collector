60615033a2c81889cac1f690611bfdc2d8ef352e
yazeedhani <haniyazeed9@gmail.com>
2022-04-18 21:31:26 -0600

    Show view for a single movie is working.

--- moviecollector/main_app/urls.py
+++ moviecollector/main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('movies/', views.movies_index, name='movies_index')
+    path('movies/', views.movies_index, name='movies_index'),
+    path('movies/<int:movie_id>/', views.movies_show, name='movies_show')
 ]
--- moviecollector/main_app/views.py
+++ moviecollector/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def index(request):
@@ -9,16 +10,10 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
-class Movie:
-    def __init__(self, name, genre, description, year):
-        self.name = name,
-        self.genre = genre,
-        self.description = description,
-        self.year = year
-
-movies = [
-    Movie('The Count of Monte Cristo', 'Adventure', "The Count of Monte Cristo' is an adaptation of the Alexander Dumas tale by the same name. Dantes, a sailor who is falsely accused of treason by his best friend Fernand, who wants Dantes' girlfriend Mercedes for himself. Dantes is imprisoned on the island prison of Chateau d'If for 13 years, where he plots revenge against those who betrayed him. With the help of another prisoner, he escapes the island and proceeds to transform himself into the wealthy Count of Monte Cristo as part of his plan to exact revenge", 2002)
-]
-
 def movies_index(request):
+    movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+def movies_show(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    return render(request, 'movies/show.html', {'movie': movie})
