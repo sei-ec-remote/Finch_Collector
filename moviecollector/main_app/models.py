1d1313325daf91e5de1669d028117a2ab8c9f6e4
mitchgemma <mitchgemma@gmail.com>
2022-04-19 21:01:27 -0400

    added templates and created model

--- moviecollector/main_app/models.py
+++ moviecollector/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
--- moviecollector/moviecollector/settings.py
+++ moviecollector/moviecollector/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -32,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main_app',
+    'moviecollector',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'moviecollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movies',
     }
 }
 
