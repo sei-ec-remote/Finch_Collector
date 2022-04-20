e43217ced804c45ede21f0dcad62a3de47d46797
mitchgemma <mitchgemma@gmail.com>
2022-04-19 21:20:30 -0400

    having trouble adding data to the database in Django shell

--- moviecollector/main_app/views.py
+++ moviecollector/main_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Movie
+
 def index(request):
   return render(request, 'index.html')
 
@@ -10,17 +12,5 @@ def about(request):
   return render(request, 'about.html')
 
 def movies_index(request):
+    movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
-
-
-class Movie:
-    def __init__(self, name, genre, description):
-        self.name = name
-        self.genre = genre
-        self.description = description
-    
-movies = [
-    Movie('Guardians of the Galaxy', 'action', 'very good'),
-    Movie('The Shawshank Redemption', 'crime', 'very good very good'),
-    Movie('The Dark Knight', 'action', 'very classic')
-]
