52b2dc22430446fe393c48285babbfeb5dc78d4a
thefrostyausty <austinrobinson0000@gmail.com>
2022-04-19 21:02:41 -0400

    CRUD the Django WAY

--- songcollector/main_app/models.py
+++ songcollector/main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    run_time = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- songcollector/main_app/urls.py
+++ songcollector/main_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name = 'about'),
+    path('songs/', views.songs_index, name='songs_index'),
+    path('songs/<int:song_id>/', views.songs_show, name='songs_show'),
+    path('songs/create/', views.SongCreate.as_view(), name='songs_create'),
+    path('songs/<int:pk>/update', views.SongUpdate.as_view(), name='songs_update'),
+    path('songs/<int:pk>/delete', views.SongDelete.as_view(), name='songs_delete'),
 ]
