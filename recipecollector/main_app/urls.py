839f43eaef584a602243916e59d75003daf967ce
katherine-kania <katherine.kania@gmail.com>
2022-04-22 18:41:38 -0400

    create page working

--- recipecollector/main_app/urls.py
+++ recipecollector/main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('recipes/', views.recipes_index, name='recipe_index'),
     path('recipes/<int:recipe_id>/', views.recipes_show, name='recipes_show'),
+    path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create')
 
 ]
--- recipecollector/main_app/views.py
+++ recipecollector/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Recipe
 
 def index(request):
@@ -30,3 +32,21 @@ def recipes_show(request, recipe_id):
   recipe = Recipe.objects.get(id=recipe_id)
   return render(request, 'recipes/show.html', {'recipe': recipe})
 
+class RecipeCreate(CreateView):
+    # use the model Recipe
+    model = Recipe
+    # utilize all fields from the model Recipe
+    # fields = '__all__'
+    fields = ['title', 'source', 'category']
+    # redirect upon successful creation
+    success_url = '/recipes'
+
+    def form_valid(self, form):
+        # creating an object from the form
+        self.object = form.save(commit=False)
+        # adding a user to that object
+        self.object.user = self.request.user
+        # saving the object in the db
+        self.object.save()
+        # redirecting to the main index page
+        return HttpResponseRedirect('/recipes')
